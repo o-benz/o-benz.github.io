4d5bf85e196a19b084051171956096cb6eb15d57
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-09-19 23:54:54 -0400

    Icons Update

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -115,8 +115,8 @@ export class MainPageComponent {
     { name: 'Music', animation: '../../../assets/music.mp4', image: '../../../assets/music.png' },
     { name: 'Running', animation: '../../../assets/running.mp4', image: '../../../assets/running.png' },
     { name: 'Working Out', animation: '../../../assets/gym.mp4', image: '../../../assets/gym.png' },
-    { name: 'Clean Eating', animation: '../../../assets/clean_eating.mp4', image: '../../../assets/clean_eating.png' },
-    { name: 'Friends & Family', animation: '../../../assets/friends_family.mp4', image: '../../../assets/friends_family.png' },
+    { name: 'Clean Eating', animation: '../../../assets/clean_eating.mp4', image: '../../../assets/eating.png' },
+    { name: 'Friends & Family', animation: '../../../assets/friends_family.mp4', image: '../../../assets/love.png' },
     { name: 'TV Shows', animation: '../../../assets/tv_shows.mp4', image: '../../../assets/tv_shows.png' }
   ];
 

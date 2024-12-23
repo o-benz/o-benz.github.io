6d18d9a2dea6a94ea8c95c95ef3f647138b09681
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-12-23 17:39:51 -0500

    feat: serious upgrades

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { collapseExpandAnimation } from 'src/assets/CollapseExpand';
 
 @Component({
@@ -10,140 +11,39 @@ import { collapseExpandAnimation } from 'src/assets/CollapseExpand';
   imports: [CommonModule],
   animations: [collapseExpandAnimation]
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
   cardStyles = {};
   isFlipped = false;
   isFlipping = false;
   isDarkMode = false;
-  
+
   isEducationCollapsed = true;
   isExperienceCollapsed = true;
   isProjectsCollapsed = true;
   isSkillsCollapsed = true;
   isInterestsCollapsed = true;
 
-  degrees = [
-    {
-      school: 'UNIVERSITÉ DE MONTRÉAL',
-      facutlty: 'ÉCOLE POLYTECHNIQUE',
-      major: 'Data Engineering and Analytics',
-      title: `MASTER OF ENGINEERING (M.Eng.)`,
-      dates: `January 2026 - September 2026`,
-      stamp: '../../../assets/stamp-master.png'
-    },
-    {
-      school: 'UNIVERSITÉ DE MONTRÉAL',
-      facutlty: 'ÉCOLE POLYTECHNIQUE',
-      major: 'Computer Engineering',
-      minor: `AI and Data Science`,
-      title: `BACHELOR OF ENGINEERING (B.Eng.)`,
-      dates: `September 2021 - December 2025`,
-      stamp: '../../../assets/stamp-bachelor.png'
-    }
-  ];
+  degrees: any[] = [];
+  experiences: any[] = [];
+  projects: any[] = [];
+  skills: any[] = [];
+  interests: any[] = [];
 
-  experiences = [
-    {
-      title: 'Data Engineering Analyst',
-      company: 'National Bank of Canada',
-      duration: 'May 2024 – Present',
-      BP1: `• Manage real-time, near real-time or batch data ETL ingestion processes through MFT, AWS and Kafka, ensuring availability and governance of certified data across analytical platforms utilizing proficiency in Python and SQL.`,
-      BP2: `• Lead Agile ceremonies as Scrum Master, drive efficient CI/CD practices through DevOps principles, and train a new team member to full operational capacity within two weeks. Resolve 93% of data flow incidents within SLA.`,
-      BP3: `• Engineer innovative solutions to streamline data ingestion workflows using Snowflake and Databricks, reducing processing times, volumes, and associated costs by nearly 30% while upholding data quality standards.`
-    },
-    {
-      title: 'Business Intelligence Intern',
-      company: 'ADM Montreal Airports',
-      duration: 'May 2023 - Sep 2023',
-      BP1: `• Develop and implement various scripts and programs in diverse languages (Python, JS, VBA) to automate menial and repetitive tasks, resulting in a 20% increase in overall operational efficiency.`,
-      BP2: `• Oversee projects, collaborate with various teams and clients, and deliver outcomes via reports and presentations.`,
-      BP3: `• Perform data analysis and processing using platforms such as Power BI, Jupyter, or MySQL to extract insights, identify patterns, and make data-driven decisions.`
-    },
-    {
-      title: 'Teacher Assistant',
-      company: 'Polytechnique Montréal',
-      duration: 'Jan 2023 - Dec 2023',
-      BP1: `• Conduct weekly in-person lab sessions for over 40 students to reinforce key concepts in C++ and OOP.`,
-      BP2: `• Grade assignments, quizzes and exams, providing detailed feedback to students on their performance.`,
-      BP3: `• Provide code debugging and troubleshooting assistance to students during and outside of office hours.`
-    },
-    {
-      title: 'Coding Instructor',
-      company: 'Collège international Marie de France',
-      duration: 'Sep 2020 - Present',
-      BP1: `• Teach the basics of programming and computer science to a group of 30 young students.`,
-      BP2: `• Design a fun and complete curriculum to foster curiosity and interest in a software-related career.`,
-      BP3: `• Develop interactive coding projects and exercises tailored to enhance problem-solving skills and creativity.`
-    }
-  ];
+  constructor(private http: HttpClient) {}
 
-  projects = [
-    {
-      title: 'Geppetto',
-      logo: '../../../assets/Geppetto.png',
-      link: 'https://github.com/o-benz/Geppetto'
-    },
-    {
-      title: 'StepByStep',
-      logo: '../../../assets/StepByStep.png',
-      link: 'https://github.com/o-benz/StepByStep'
-    },
-    {
-      title: 'ROBO-QUEST',
-      logo: '../../../assets/ROBOQUEST.png',
-      link: 'https://github.com/o-benz/ROBO-QUEST'
-    },
-    {
-      title: 'Mealer',
-      logo: '../../../assets/Mealer.png',
-      link: 'https://github.com/o-benz/Mealer'
-    },
-    {
-      title: 'SmartyShowdown',
-      logo: '../../../assets/SmartyShowdown.png',
-      link: 'https://github.com/o-benz/SmartyShowdown'
-    },
-    {
-      title: 'The Polynator',
-      logo: '../../../assets/Polynator.png',
-      link: 'https://github.com/o-benz/The-Polynator'
-    },
-    {
-      title: 'OverEngineered',
-      logo: '../../../assets/OverEngineered.png',
-      link: 'https://github.com/o-benz/OverEngineered'
-    }
-  ];
+  ngOnInit(): void {
+    this.loadData('degrees', 'assets/data/education.json');
+    this.loadData('experiences', 'assets/data/experience.json');
+    this.loadData('projects', 'assets/data/projects.json');
+    this.loadData('skills', 'assets/data/skills.json');
+    this.loadData('interests', 'assets/data/interests.json');
+  }
 
-  skills = [
-    { name: 'Python', level: 'Advanced' },
-    { name: 'C/C++', level: 'Advanced' },
-    { name: 'SQL', level: 'Advanced' },
-    { name: 'JS/TS', level: 'Advanced' },
-    { name: 'HTML', level: 'Advanced' },
-    { name: 'CSS/SCSS', level: 'Advanced' },
-    { name: 'R', level: 'Advanced' },
-    { name: 'Angular', level: 'Advanced' },
-    { name: 'React', level: 'Intermediate' },
-    { name: 'Node.js', level: 'Advanced' },
-    { name: '.NET', level: 'Intermediate' },
-    { name: 'MongoDB', level: 'Intermediate' },
-    { name: 'AWS', level: 'Advanced' },
-    { name: 'Snowflake', level: 'Advanced' },
-    { name: 'Databricks', level: 'Intermediate' },
-    { name: 'Power BI', level: 'Intermediate' },
-    { name: 'Excel', level: 'Advanced' },
-  ];
-
-  interests = [
-    { name: 'Martial Arts', animation: '../../../assets/boxing.mp4', image: '../../../assets/boxing.png' },
-    { name: 'Music', animation: '../../../assets/music.mp4', image: '../../../assets/music.png' },
-    { name: 'Running', animation: '../../../assets/running.mp4', image: '../../../assets/running.png' },
-    { name: 'Working Out', animation: '../../../assets/gym.mp4', image: '../../../assets/gym.png' },
-    { name: 'Clean Eating', animation: '../../../assets/clean_eating.mp4', image: '../../../assets/eating.png' },
-    { name: 'Friends & Family', animation: '../../../assets/friends_family.mp4', image: '../../../assets/love.png' },
-    { name: 'TV Shows', animation: '../../../assets/tv_shows.mp4', image: '../../../assets/tv_shows.png' }
-  ];
+  private loadData(key: string, url: string): void {
+    this.http.get(url).subscribe((data) => {
+      (this as any)[key] = data;
+    });
+  }
 
   @HostListener('window:resize', ['$event'])
   onResize() {
